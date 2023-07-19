8d04a4a54ff6f74d3822517bcf393f710bc5e2cb
letai2001 <nguyenletai16122001@gmail.com>
2023-07-19 18:38:58 +0700

    crawlLinkPost2

--- fix_csv.py
+++ fix_csv.py
@@ -6,11 +6,12 @@ filtered_links = []
 with open('output.csv', 'r', newline='', encoding='utf-8') as file:
     reader = csv.reader(file)
     for row in reader:
-        if row[0].startswith("https://www.facebook.com/fabrizioromanoherewego/posts/"):
+        if row[0].startswith("https://www.facebook.com/fabrizioromanoherewego/posts"):
             filtered_links.append(row[0])
 
 # Ghi các liên kết đã lọc vào một file mới (hoặc ghi đè lên file hiện tại nếu bạn muốn)
 with open('filtered_output.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
+    writer.writerow(['link_post'])  # Ghi tiêu đề cột vào dòng đầu tiên của file
     for link in filtered_links:
         writer.writerow([link])
