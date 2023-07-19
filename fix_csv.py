0d224eb32d8afe5ca197cbb218410c71f1cf4376
letai2001 <nguyenletai16122001@gmail.com>
2023-07-19 17:24:38 +0700

    crawlLinkPost

---
+++ fix_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+filtered_links = []
+
+# Đọc file CSV hiện tại và lưu các liên kết thỏa mãn vào danh sách filtered_links
+with open('output.csv', 'r', newline='', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        if row[0].startswith("https://www.facebook.com/fabrizioromanoherewego/posts/"):
+            filtered_links.append(row[0])
+
+# Ghi các liên kết đã lọc vào một file mới (hoặc ghi đè lên file hiện tại nếu bạn muốn)
+with open('filtered_output.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    for link in filtered_links:
+        writer.writerow([link])
